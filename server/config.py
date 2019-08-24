8b77369ba0066db68d2e99b1a5a4572fc081044e
minhyeoklee <rhodochrosited@gmail.com>
2019-08-25 04:50:34 +0900

    new config.py

--- server/config.py
+++ server/config.py
@@ -1,14 +1,27 @@
 import os
+import logging
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-class Config(object):
-    """Basic Configuration"""
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+class Logger(object):
+    # DEBUG - INFO - WARN - ERROR - CRITICAL
+    level = "INFO"
+
 
 class DataBase(object):
-    """DataBase Configuration"""
     host = 'localhost'
     port = 27017
     username = 'mhlee'
     password = 'mhlee'
     db_name = 'chatbot'
+
+
+class Flask(object):
+    host = '0.0.0.0'
+    port = 8000
+    debug = True
+    version = 0.1
+    title = 'hanyang-chatbot-dev'
+    desc = 'development version'
