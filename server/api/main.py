4e786e7e71744bdd5491773e10520f69451ec9b4
minhyeoklee <rhodochrosited@gmail.com>
2019-10-17 21:03:20 +0900

    Save user input to firestore

--- server/api/main.py
+++ server/api/main.py
@@ -1,15 +1,23 @@
 from lib.flask_restplus import Resource, Namespace
 from engine.main import Chatbot
+from db.connect import db
+from db.user_input import UserInput
 
 # from engine.preprocessor.morph import analyze
 
 # chatbot = Chatbot()
 ns = Namespace(name='', description='Restful API')
 
+user_input = db.collection('user_input')
+
 
 @ns.route('/<string:text>')
 class Index(Resource):
 
-    @ns.doc('챗봇 입력', params={'text': '입력'})
+    @ns.doc('챗봇 입력')
     def get(self, text):
+        ui = UserInput(text=text)
+
+        user_input.add(ui.to_dict())
+
         return text
