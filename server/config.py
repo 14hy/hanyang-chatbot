36580dcfb0b714931795a696553fa1a9a02319b8
minhyeok.lee <minhyeok.lee95@gmail.com>
2020-03-16 03:50:25 +0900

    Mhlee (#18)
    * :fire: travis related files
    
    * :new: Basic pytests
    
    * :new: Jenkins file
    
    * :wrench: Add tests and update shuttle
    
    * :new: Add QA CRUD
    
    * :new: Userinput get
    
    * :up: Header section in swagger
    
    * :wrench: Fix miner
    
    * :wrench: Jenkins file
    
    * :up: flask_restplus -> flask_restx
    
    * :new: apply https certs and moved to mhlee.engineer

--- server/config.py
+++ server/config.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -18,7 +19,7 @@ class Config(object):
 
     # FireStore
     PROJECT_ID = "cool-benefit-185923"
-    SERVICE_ACCOUNT = ""
+    SERVICE_ACCOUNT = f"{BASE_DIR}/cert/service_account.json"
 
     # paths
     SHUTTLE_DIR = "shuttle_files"
@@ -28,22 +29,22 @@ class DevConfig(Config):
     LOG_LEVEL = "DEBUG"
 
     # FLASK
-    HOSTS = "0.0.0.0"
+    HOST = "0.0.0.0"
     PORT = 8001
     DEBUG = True
     TESTING = True
-    VERSION = 0.3
+    VERSION = 0.4
     TITLE = "hanyang-chatbot-api"
     DESC = ""
     SECRET_KEY = open(f"{BASE_DIR}/cert/secret_key", mode="r").readline()
     JWT_SECRET_KEY = SECRET_KEY
 
-    # FireStore
-    PROJECT_ID = ""
-    SERVICE_ACCOUNT = f"{BASE_DIR}/cert/service_account.json"
-
     # paths
     SHUTTLE_DIR = f"{BASE_DIR}/shuttle_files"
+
+    # login
+    LOGIN_TOKEN = "baf06612f9fc2e34694ca1b4254e4a8f368ea6c80f0f901743062a3a482b17ff"
+    JWT_EXPIRES = datetime.timedelta(days=365)
 
 
 class ProdConfig(Config):
@@ -51,20 +52,20 @@ class ProdConfig(Config):
 
     # FLASK
     HOST = "0.0.0.0"
-    PORT = 8001
+    PORT = 5000
     DEBUG = False
-    VERSION = 0.3
+    VERSION = 0.4
     TITLE = "hanyang-chatbot-api"
     DESC = ""
     SECRET_KEY = open(f"{BASE_DIR}/cert/secret_key", mode="r").readline()
     JWT_SECRET_KEY = SECRET_KEY
 
-    # FireStore
-    PROJECT_ID = "cool-benefit-185923"
-    SERVICE_ACCOUNT = ""
-
     # paths
     SHUTTLE_DIR = f"{BASE_DIR}/shuttle_files"
+
+    # login
+    LOGIN_TOKEN = "4373671fc1e3dd6517f264c30e70e904016f668b422b210f7083453eec1b722d"
+    JWT_EXPIRES = datetime.timedelta(hours=1)
 
 
 if __name__ == "__main__":
--- server/deploy.sh
+++ server/deploy.sh
@@ -1,1 +1,6 @@
-gcloud app deploy --quiet
+#!/usr/bin/env bash
+branch=$1
+
+git fetch --all
+git reset --hard origin/"$branch"
+docker run -d -v "$PWD":/app -p 5000:5000 -e env=prod --name hanyang-chatbot hanyang-chatbot python app.py
