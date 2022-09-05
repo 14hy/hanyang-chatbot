effe2bf1e7b199c2e3afe190b812a0ee4face687
Minhyeok Lee <minhyeok.lee95@gmail.com>
2022-09-06 00:25:07 +0900

    Update notice

--- server/api/services/notice.py
+++ server/api/services/notice.py
@@ -13,16 +13,28 @@ parser.add_argument(
 )
 
 NOTICE = None
+DEFAULT_NOTICE = """
+안녕하냥~ 하냥이다냥~!! <br>
+<img src="https://blog.mhlee.dev/wp-content/uploads/2021/03/image-4.png"> <br>
+개선사항이 있으면 스토어에서 리뷰를 써달라냥!! 🙏<br><br>
+그리고!! <br>
+셔틀 시간표가 자주 바뀌므로 😭 <br>
+공식 시간표 및 공지도 참고해달라냥! <br>
+<img src="https://www.hanyang.ac.kr/documents/20182/6049116/20190923-bus3.png"> <br>
+
+
+https://www.hanyang.ac.kr/<br>web/www/shuttle_bus_timetable <br>
+"""
+
 
 @ns_notice.route("/")
 class Bus(Resource):
     @ns_notice.doc(
-        "태욱아 받아라",
         params={
         },
     )
     def get(self):
-        return NOTICE if NOTICE else None
+        return NOTICE if NOTICE else DEFAULT_NOTICE
 
     @ns_notice.doc(
         "공지 등록.",
